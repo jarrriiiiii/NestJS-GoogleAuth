88961b83b1fc1c49c76e1c9a193e68d1e1a66093
jarrriiiiii <79092437+jarrriiiiii@users.noreply.github.com>
2023-02-20 01:23:15 +0500

    Main.ts

---
+++ Main.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');  //The endpoint of URL will have this word localhost:3001/api/
+  await app.listen(3001);
+}
+bootstrap();
+
+
+//nest g module auth - To create the module directly from the CLI
